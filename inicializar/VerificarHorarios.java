b47293802d152d5e813c0a74603ebbbec63bff65
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-11-26 04:07:33 -0300

    Introduccion

--- inicializar/VerificarHorarios.java
+++ inicializar/VerificarHorarios.java
@@ -3,7 +3,7 @@ package inicializar;
 public class VerificarHorarios {
 
 	/**
-	 * @author Diego J D Arias
+	 * @author Diego J D Arias 
 	 */
 	public static void main(String[] args) {
 		Horario h = new Horario(2, 8, 45, 12, 45, 3);
