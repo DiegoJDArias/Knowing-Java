6decc7aab44759dcbb64c6a64d0a5863227d47e5
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-11-29 04:31:57 -0300

    Introduccion

--- Introducción/src/HolaMundo.java
+++ Introducción/src/HolaMundo.java
@@ -1,5 +1,9 @@
 public class HolaMundo {
 
+/* /////////////////////////////////////////////////
+   Author Diego J D Arias - diegojdarias@gmail.com.
+*/////////////////////////////////////////////////
+
 	public static void main(String[] args) {
 		// Mostrar por consola "Hola Mundo"
 		System.out.println("Hola Mundo!!!");
