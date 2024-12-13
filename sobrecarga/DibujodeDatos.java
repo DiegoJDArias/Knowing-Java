faa71b6772d0d5dd342d1702445668e61fccf04d
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-12-13 00:51:11 -0300

    Clases

---
+++ sobrecarga/DibujodeDatos.java
@@ -0,0 +1,24 @@
+package sobrecarga;
+
+/* /////////////////////////////////////////////////
+   Author Diego J D Arias - diegojdarias@gmail.com.
+*/////////////////////////////////////////////////
+
+public class DibujodeDatos {
+	void dibujar (String s) {
+		/*
+		 * . . .
+		 */
+		}
+		void dibujar (int i) {
+		/*
+		 * . . .
+		 */
+		}
+		void dibujar (float f) {
+		/*
+		 * . . .
+		 */
+		}
+
+}
